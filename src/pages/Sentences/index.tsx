3e9e99560745f7d218a43f98b73a2f2687d9920e
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-04-09 01:07:50 +0900

    Feat: sentenceList 전역 상태로 저장

--- src/pages/Sentences/index.tsx
+++ src/pages/Sentences/index.tsx
@@ -4,15 +4,17 @@ import { Container, SentenceList } from './styles';
 import NavBar from '../../components/NavBar/NavBar';
 import TopBar from '../../components/TopBar';
 import SentenceItem from './SentenceItem';
-import { SentenceItemDTO } from '../../apis/topics/dto';
 import { getSentenceListApi } from '../../apis/topics';
+import { useRecoilState } from 'recoil';
+import { sentenceListState } from '../../recoil/atoms/sentenceListAtom';
 
 const SentencesPage: React.FC = () => {
   const { topicId } = useParams<{ topicId: string }>();
 
   const location = useLocation();
   const topicName = location.state?.topicName || 'Practice Sentences';
-  const [sentences, setSentences] = useState<SentenceItemDTO[]>([]);
+
+  const [sentenceList, setSentenceList] = useRecoilState(sentenceListState);
 
   // topicId가 undefined일 경우 대비
   const topicIdNum = topicId ? Number(topicId) : null;
@@ -22,7 +24,7 @@ const SentencesPage: React.FC = () => {
     try {
       const response = await getSentenceListApi(Number(topicId));
       if (response.success) {
-        setSentences(response.data);
+        setSentenceList(response.data);
       }
     } catch (error) {
       console.error('Error fetching sentence list:', error);
@@ -37,7 +39,7 @@ const SentencesPage: React.FC = () => {
     <Container>
       <TopBar name={topicName} />
       <SentenceList>
-        {sentences.map((sentence) => (
+        {sentenceList.map((sentence) => (
           <SentenceItem
             key={sentence.id}
             korean={sentence.korean}
