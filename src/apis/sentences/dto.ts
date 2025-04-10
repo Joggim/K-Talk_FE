a363dd6abbe2332c87c80394d85a55903788b583
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-04-10 16:00:04 +0900

    Feat: 발음 피드백 요청 api 모듈화

--- src/apis/sentences/dto.ts
+++ src/apis/sentences/dto.ts
@@ -1,10 +1,26 @@
 import { BaseSuccessResponse } from '../util/dto';
 
 export type GetSentenceResponse = BaseSuccessResponse<GetSentenceData>;
+export type FeedbackResponse = BaseSuccessResponse<FeedbackResponseData>;
 
 export interface GetSentenceData {
   id: number;
   korean: string;
   translation: string;
   audioUrl: string;
+}
+
+export interface PronunciationError {
+  wrong: string;
+  correct: string;
+  index: number;
+}
+
+export interface FeedbackResponseData {
+  sentenceId: number;
+  passed: boolean;
+  userText: string;
+  userAudioUrl: string;
+  pronunciationErrors: PronunciationError[];
+  feedBack?: string;
 }
