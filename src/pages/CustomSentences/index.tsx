94ebcc4e9a4ea58d5067d82f5da2fbd1f3d5d43c
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-04-30 13:13:55 +0900

    Feat: CustomSentencesPage에서 PracticePage로의 네비게이션 구현

--- src/pages/CustomSentences/index.tsx
+++ src/pages/CustomSentences/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Container,
   ContentArea,
@@ -10,9 +10,32 @@ import TopBar from '../../components/TopBar';
 import SentenceItem from '../../components/SentenceItem';
 import { StyledText } from '../../components/StyledText/StyledText.styles';
 import theme from '../../styles/theme';
+import { useRecoilState } from 'recoil';
+import { sentenceListState } from '../../recoil/atoms/sentenceListAtom';
 import { dummyCustomSentences } from './dummyCustomSentences';
 
 const CustomSentencesPage: React.FC = () => {
+  const [sentenceList, setSentenceList] = useRecoilState(sentenceListState);
+
+  // 문장 리스트 조회 API
+  const getCustomSentenceList = async () => {
+    /*
+    try {
+      const response = await getCustomSentenceListApi(Number(customTrainingId));
+      if (response.success) {
+        setSentenceList(response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching sentence list:', error);
+    }
+    */
+    setSentenceList(dummyCustomSentences.sentences);
+  };
+
+  useEffect(() => {
+    getCustomSentenceList();
+  }, []);
+
   return (
     <Container>
       <TopBar />
@@ -33,7 +56,7 @@ const CustomSentencesPage: React.FC = () => {
           Try Saying This
         </StyledText>
 
-        {dummyCustomSentences.sentences.map((sentence) => (
+        {sentenceList.map((sentence) => (
           <SentenceItem
             key={sentence.id}
             korean={sentence.korean}
