a9eacc8c2ab9fbff94cdb62dfe95fa7978e4eefb
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2025-05-20 17:37:55 +0900

    Feat: MoreBtn 공통 컴포넌트 구현

---
+++ src/components/MoreBtn/styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const MoreBtn = styled.button`
+  display: flex;
+  padding: 4px 0px;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  padding-left: 10px;
+`;
